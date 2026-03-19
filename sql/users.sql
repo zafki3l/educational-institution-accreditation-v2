33486b137491d2121ccb9132862cfaac0750d78c
Th41 <phamdinhthai2005@gmail.com>
2026-03-20 00:18:57 +0700

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,1 @@
-INSERT INTO educational_institution_accreditation.users
-(id, first_name, last_name, email, password, role_id, created_at, updated_at, department_id)
-VALUES('31ac1c24-260a-4e0d-a465-4e6019ac3fb3', 'Phạm Đình', 'Thái', 'th42@abc.com', '$2y$10$aVtCUzGYo2Cbb65mtI.bFOVjEDp8oZS3oBQ0CAfTWn/FPcDbU5HvS', 3, CURRENT_TIMESTAMP, '', '');
 
--- Đăng nhập tài khoản admin: 
--- email: th42@abc.com
--- password: phamdinhthai123 

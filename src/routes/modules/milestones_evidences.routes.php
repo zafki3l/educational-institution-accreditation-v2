1b990705ac4eefd8884ea9378f1183a516fc2aec
Th41 <phamdinhthai2005@gmail.com>
2026-04-01 17:41:15 +0000

    add docker-compose.prod file

--- src/routes/modules/milestones_evidences.routes.php
+++ src/routes/modules/milestones_evidences.routes.php
@@ -3,7 +3,14 @@
 use App\Modules\QualityAssessment\Presentation\Controllers\MilestoneEvidence\CreateMilestoneEvidenceController;
 use App\Modules\QualityAssessment\Presentation\Controllers\MilestoneEvidence\DeleteMilestoneEvidenceController;
 use App\Modules\QualityAssessment\Presentation\Controllers\MilestoneEvidence\IndexMilestoneEvidenceController;
+use App\Shared\Web\Middlewares\EnsureAuth;
+use App\Shared\Web\Middlewares\EnsureStaff;
 
-$route->get('/api/evidences/{id}/criterias', [IndexMilestoneEvidenceController::class, 'index']);
-$route->post('/api/evidences/{id}/criterias', [CreateMilestoneEvidenceController::class, 'store']);
-$route->delete('/api/evidences/criterias', [DeleteMilestoneEvidenceController::class, 'destroy']);
+$route->middleware([EnsureAuth::class, EnsureStaff::class])
+    ->get('/api/evidences/{id}/criterias', [IndexMilestoneEvidenceController::class, 'index']);
+
+$route->middleware([EnsureAuth::class, EnsureStaff::class])
+    ->post('/api/evidences/{id}/criterias', [CreateMilestoneEvidenceController::class, 'store']);
+
+$route->middleware([EnsureAuth::class, EnsureStaff::class])
+    ->delete('/api/evidences/criterias', [DeleteMilestoneEvidenceController::class, 'destroy']);

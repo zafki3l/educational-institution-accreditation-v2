f15acea906488fee7ed496aac19d906f92b714e7
Th41 <phamdinhthai2005@gmail.com>
2026-04-11 09:36:29 +0000

    daspodajsdas

--- test_load.js
+++ test_load.js
@@ -2,8 +2,12 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 500, 
-  duration: '30s',
+  stages: [
+    { duration: '1m', target: 200 },
+    { duration: '1m', target: 500 },
+    { duration: '2m', target: 500 },
+    { duration: '1m', target: 0 },
+  ],
 };
 
 export default function () {
